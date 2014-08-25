0f5593745b6ed71a5fd4727c7c94562a2392efa5
hugoizquierdo <hugo_id2004@hotmail.com>
2014-08-25 18:02:48 +0200

    my second change

--- app/src/main/java/com/myapplication/MyActivity.java
+++ app/src/main/java/com/myapplication/MyActivity.java
@@ -12,9 +12,10 @@ public class MyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        //dfddf
     }
-
-
+//my first change
+//hola
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
